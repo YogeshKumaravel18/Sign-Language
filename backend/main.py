dbad122e6e8d6edcf19d1f90fff3c10356aa18ba
Yogesh Kumaravel <116558531+YogeshKumaravel18@users.noreply.github.com>
2026-03-24 19:57:05 +0530

    Implement FastAPI for sign language prediction

---
+++ backend/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+import mediapipe as mp
+from tensorflow.keras.models import load_model
+from PIL import Image
+import io
+
+app = FastAPI()
+
+# Load model
+model = load_model("model/sign_language_model.h5")
+
+labels = ['enna pannura', 'vanakam']
+
+# Mediapipe
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1)
+
+@app.get("/")
+def home():
+    return {"message": "API Running 🚀"}
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    frame = np.array(image)
+
+    rgb = frame.copy()
+    result = hands.process(rgb)
+
+    if result.multi_hand_landmarks:
+        hand = result.multi_hand_landmarks[0]
+
+        landmark_list = []
+        for lm in hand.landmark:
+            landmark_list.extend([lm.x, lm.y, lm.z])
+
+        landmarks = np.array(landmark_list)
+
+        # Normalize
+        landmarks = landmarks - np.min(landmarks)
+        if np.max(landmarks) != 0:
+            landmarks = landmarks / np.max(landmarks)
+
+        landmarks = landmarks.reshape(1, -1)
+
+        prediction = model.predict(landmarks)
+        class_id = np.argmax(prediction)
+        confidence = float(np.max(prediction))
+
+        return {
+            "prediction": labels[class_id],
+            "confidence": confidence
+        }
+
+    return {"error": "No hand detected"}
